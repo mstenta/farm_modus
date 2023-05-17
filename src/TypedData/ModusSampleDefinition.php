06bf306d06af1f1b58f836401ca4b3ea674083f8
Paul Weidner <paul.weidner@gmail.com>
2023-05-17 09:41:06 -0700

    Add base ModusSampleDefinition class

---
+++ src/TypedData/ModusSampleDefinition.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\farm_modus\TypedData;
+
+use Drupal\Core\TypedData\ComplexDataDefinitionBase;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Base class for modus sample data definitions.
+ */
+abstract class ModusSampleDefinition extends ComplexDataDefinitionBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getPropertyDefinitions() {
+    $properties = [];
+    $properties['id'] = DataDefinition::create('string');
+    $properties['labid'] = DataDefinition::create('string');
+    $properties['fmisid'] = DataDefinition::create('string');
+    $properties['collection_date'] = DataDefinition::create('datetime_iso8601');
+
+    // @todo Add geolocation type.
+    $properties['geolocation'] = DataDefinition::create('string');
+
+    return $properties;
+  }
+
+}
